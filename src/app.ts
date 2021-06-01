92b13d65d31c7e64cc496f36362598f6addf952d
Thaqib m <thaqibdamani0@gmail.com>
2021-06-01 19:03:03 +0530

    compile router

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express, {Request, Response, Application, NextFunction} from 'express';
 import { compileRouter } from './routes/compile_routs';
-import { interp } from 'simpl_interp';
+
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 5000; 
 const app:Application = express(); 
 
 app.use(express.static('build'));
+app.use(bodyParser.json());
 app.use('/compile', compileRouter);
-
 
 app.listen(PORT, ()=>console.log("Server running on http://localhost:5000/"));
--- src/routes/compile_routs.ts
+++ src/routes/compile_routs.ts
@@ -1,7 +1,16 @@
 import express, {Router, Request, Response, Application, NextFunction}  from 'express'; 
-
+import {interp} from 'simpl_interp';
 export const compileRouter : Router = express.Router(); 
 
 compileRouter.get('/', (req:Request,res:Response,next:NextFunction) =>{
     res.status(200).send("Simpl Compiler");
+});
+
+compileRouter.post('/', (req: Request, res:Response, next: NextFunction) =>{
+    console.log(req.body);
+    const out = interp(req.body['code'])
+    .then(output => {
+        res.send({"output": output, "error": null});
+    })
+    .catch(e => res.send({"output": null, "error": e}));
 });
